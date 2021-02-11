7433479c79757cdcd818b522325068876316de9a
Ihab Zeedia <ihab.zeedia@gmail.cm>
2021-02-11 13:27:39 -0800

    initiale commit.

--- GoogleStorage.php
+++ GoogleStorage.php
@@ -2,20 +2,157 @@
 
 namespace Stanford\GoogleStorage;
 
-use ExternalModules\ExternalModules;
-
 require_once "emLoggerTrait.php";
+require __DIR__ . '/vendor/autoload.php';
+
+# Imports the Google Cloud client library
+use Google\Cloud\Storage\StorageClient;
+use Google\Cloud\Storage\Bucket;
 
 /**
-* Class GoogleStorage
-* @package Stanford\GoogleStorage
-*
-*
-*/
+ * Class GoogleStorage
+ * @package Stanford\GoogleStorage
+ * @property \Google\Cloud\Storage\StorageClient $client
+ * @property \Google\Cloud\Storage\Bucket[] $buckets
+ * @property array $instances
+ * test
+ */
 class GoogleStorage extends \ExternalModules\AbstractExternalModule
 {
 
     use emLoggerTrait;
+
+
+    /**
+     * @var \Google\Cloud\Storage\StorageClient
+     */
+    private $client;
+
+    /**
+     * @var \Google\Cloud\Storage\Bucket[]
+     */
+    private $buckets;
+
+    /**
+     * @var array
+     */
+    private $instances;
+
+    public function __construct()
+    {
+        try {
+            parent::__construct();
+
+            if (isset($_GET['pid']) && $this->getProjectSetting('google-api-token') != '' && $this->getProjectSetting('google-project-id') != '') {
+                $this->setInstances();
+
+                //configure google storage object
+                $this->setClient(new StorageClient(['keyFile' => json_decode($this->getProjectSetting('google-api-token'), true), 'projectId' => $this->getProjectSetting('google-project-id')]));
+
+                if (!empty($this->getInstances())) {
+                    $buckets = array();
+                    foreach ($this->getInstances() as $instance) {
+                        $buckets[$instance['google-storage-bucket']] = $this->getClient()->bucket($instance['google-storage-bucket']);
+                    }
+                    $this->setBuckets($buckets);
+                }
+            }
+        } catch (\Exception $e) {
+            #echo $e->getMessage();
+        }
+    }
+
+    /**
+     * @param \Google\Cloud\Storage\Bucket $bucket
+     * @param string $objectName
+     * @param int $duration
+     * @return string
+     * @throws \Exception
+     */
+    public function getGoogleStorageSignedUrl($bucket, $objectName, $duration = 50)
+    {
+        $url = $bucket->object($objectName)->signedUrl(new \DateTime('+ ' . $duration . ' seconds'),
+            [
+                'version' => 'v4',
+            ]);
+        return $url;
+    }
+
+    /**
+     * @param \Google\Cloud\Storage\Bucket $bucket
+     * @param string $objectName
+     * @param int $duration
+     * @return string
+     * @throws \Exception
+     */
+    public function getGoogleStorageSignedUploadUrl($bucket, $objectName, $duration = 3600)
+    {
+        $url = $bucket->object($objectName)->signedUrl(new \DateTime('+ ' . $duration . ' seconds'),
+            [
+                'method' => 'PUT',
+                'contentType' => 'application/octet-stream',
+                'version' => 'v4',
+            ]);
+        return $url;
+    }
+
+    /**
+     * @return \Google\Cloud\Storage\StorageClient
+     */
+    public function getClient()
+    {
+        return $this->client;
+    }
+
+    /**
+     * @param \Google\Cloud\Storage\StorageClient $client
+     */
+    public function setClient(StorageClient $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * @param string $bucketName
+     * @return \Google\Cloud\Storage\Bucket
+     */
+    public function getBucket($bucketName)
+    {
+        return $this->getBuckets()[$bucketName];
+    }
+
+    /**
+     * @return Bucket[]
+     */
+    public function getBuckets()
+    {
+        return $this->buckets;
+    }
+
+    /**
+     * @param Bucket[] $buckets
+     */
+    public function setBuckets(array $buckets)
+    {
+        $this->buckets = $buckets;
+    }
+
+
+    /**
+     * @return array
+     */
+    public function getInstances()
+    {
+        return $this->instances;
+    }
+
+    /**
+     */
+    public function setInstances()
+    {
+        $this->instances = $this->getSubSettings('instance', $this->getProjectId());
+    }
+
 
     /******************************************************************************************************************/
     /* HOOK METHODS                                                                                                   */
--- src/test.php
+++ src/test.php
@@ -2,8 +2,20 @@
 namespace Stanford\GoogleStorage;
 /** @var \Stanford\GoogleStorage\GoogleStorage $module */
 
+try {
+    $bucket = $module->getBucket('redcap-storage-test');
+    $response = $module->getGoogleStorageSignedUploadUrl($bucket, 'config.json');
 
+    print('Generated PUT signed URL:' . PHP_EOL);
+    print($response . PHP_EOL);
+    print('You can use this URL with any user agent, for example:' . PHP_EOL);
+    print("curl -X PUT -H 'Content-Type: application/octet-stream' " .
+        '--upload-file my-file ' . $response . PHP_EOL);
+} catch (\Exception $e) {
+    echo $e->getMessage();
+}
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -11,7 +23,8 @@ namespace Stanford\GoogleStorage;
     <title>Google Storage</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css"/>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
@@ -19,5 +32,48 @@ namespace Stanford\GoogleStorage;
 </header>
 <body>
 <h2>Google Storage Test</h2>
+<form id="test-form" enctype="multipart/form-data">
+    <input name="file" type="file"/>
+    <input type="button" value="Upload"/>
+</form>
+<progress></progress>
+<script>
+
+    $(':button').on('click', function () {
+        var data = new FormData($('#test-form')[0]);
+        console.log(data)
+        $.ajax({
+            // Your server script to process the upload
+            url: "<?php echo $response ?>",
+            type: 'PUT',
+
+            // Form data
+            data: data,
+
+            // Tell jQuery not to process data or worry about content-type
+            // You *must* include these options!
+            processData: false,
+            contentType: false,
+            // Custom XMLHttpRequest
+            xhr: function () {
+                var myXhr = $.ajaxSettings.xhr();
+                if (myXhr.upload) {
+                    // For handling the progress of the upload
+                    myXhr.upload.addEventListener('progress', function (e) {
+                        if (e.lengthComputable) {
+                            $('progress').attr({
+                                value: e.loaded,
+                                max: e.total,
+                            });
+                        }
+                    }, false);
+                }
+                return myXhr;
+            }
+        });
+    });
+</script><!-- end container -->
 </body>
 </html>
+
+
